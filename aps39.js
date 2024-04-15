860b01fdbe8604f27604433d58604c7f517d0258
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-04-15 12:16:36 +0400

    commit

--- aps39.js
+++ aps39.js
@@ -1,28 +1,60 @@
-function elaveet() {
-    let table = document.getElementById("tablem").getElementsByTagName('thead')[0];
-    let newTable = table.insertRow(table.rows.length);
-    let cell1 = newTable.insertCell(0);
-    let cell2 = newTable.insertCell(1);
-    let cell3 = newTable.insertCell(2);
-    let cell4 = newTable.insertCell(3);
-    let cell5 = newTable.insertCell(4);
 
-    let numbers = table.rows.length;
-    cell1.innerHTML = numbers + -1;
-    cell2.innerHTML = "<input type='text' class='name' placeholder='Ad'>";
-    cell3.innerHTML = "<input type='text' class='surname' placeholder='Soyad'>";
-    cell4.innerHTML = "<input type='text' class='age' placeholder='Yas'>";
-    cell5.innerHTML= '<button id="onbutton" onclick="removeRow(this)">Sil</button>' +
-    '<button id="offbutton" onclick="saveRow(this)">Kaydele</button>'
+const addBtn = document.querySelector(".addBtn");
 
-}
+const tbody = document.querySelector("tbody");
+let allow = true;
 
-function removeRow(button) {
-    let row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-}
+const orderRow = () => {
+  const rows = [...document.querySelectorAll("tbody tr")];
+  rows.map((row, key) => {
+    [(row.querySelector("td").textContent = key + 1)];
+  });
+};
 
-function saveRow(button) {
+const saveData = (e) => {
 
-}
+  const inputs = [...document.querySelectorAll("input")];
+  inputs.map((input) => {
+    input.parentElement.textContent = input.value;
+  });
+  e.target.textContent = "Düzəliş et";
+  allow = true;
+};
 
+addBtn.addEventListener("click", () => {
+  if (!allow) {
+    alert("Öncəki xananı doldurun pls...");
+    return;
+  }
+
+  allow = false;
+  const row = document.createElement("tr");
+  const noTd = document.createElement("td");
+  const nameTd = document.createElement("td");
+  const nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("placeholder", "Ad");
+  nameTd.append(nameInput);
+  const surnameTd = document.createElement("td");
+  const surnameInput = document.createElement("input");
+  surnameInput.setAttribute("type", "text");
+  surnameInput.setAttribute("placeholder", "Soyad");
+  surnameTd.append(surnameInput);
+  const ageTd = document.createElement("td");
+  const ageInput = document.createElement("input");
+  ageInput.setAttribute("type", "number");
+  ageInput.setAttribute("placeholder", "Yaş");
+  ageTd.append(ageInput);
+  const optionsTd = document.createElement("td");
+  const saveBtn = document.createElement("button");
+  saveBtn.textContent = "Yadda saxla";
+  saveBtn.classList.add("saveBtn");
+  saveBtn.addEventListener("click", saveData);
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Sil";
+  cancelBtn.classList.add("cancelBtn");
+  optionsTd.append(saveBtn, cancelBtn);
+  row.append(noTd, nameTd, surnameTd, ageTd, optionsTd);
+  tbody.append(row);
+  orderRow();
+});
